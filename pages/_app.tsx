ca230c9bd287a803ca68cc16a16c9d78c1c67206
Vencer Torres <vencer.torres@gmail.com>
2022-07-22 20:19:56 +0800

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
